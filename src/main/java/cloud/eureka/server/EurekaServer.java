90899794730e20b29b6f4a80f6fab5ba02937126
linhucha <msl_2015@163.com>
2017-02-13 10:34:24 +0800

    Update EurekaServer.java

--- src/main/java/cloud/eureka/server/EurekaServer.java
+++ src/main/java/cloud/eureka/server/EurekaServer.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * 包      名 ：  cloud.eureka.server
  * 文 件 名 : EurekaServer
  * 描       述: TODO
- * 作        者： 超哥
+ * 作        者： lyj
  * 创建时间： 2016年7月30日 下午6:00:23
  * 版         本：2.0
  */
